ccbcbb1a60263c1fb1c4ebbd31278c97c12e5c20
cagla.boynuegri <cagla.boynuegri@scalefocus.com>
2024-12-30 15:18:25 +0300

    refactored package names for spring cloud project

--- src/main/java/guru/sfg/beer/order/service/services/listeners/ValidationResultListener.java
+++ src/main/java/guru/sfg/beer/order/service/services/listeners/ValidationResultListener.java
@@ -5,6 +5,7 @@ import guru.sfg.beer.order.service.services.BeerOrderManager;
 import guru.sfg.brewery.model.events.ValidateOrderResult;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
@@ -19,7 +20,7 @@ public class ValidationResultListener {
 
     private final BeerOrderManager beerOrderManager;
 
-    //@JmsListener(destination = JmsConfig.VALIDATE_ORDER_RESPONSE_QUEUE)
+    @JmsListener(destination = JmsConfig.VALIDATE_ORDER_RESPONSE_QUEUE)
     public void listen(ValidateOrderResult result){
         final UUID beerOrderId = result.getOrderId();
 
